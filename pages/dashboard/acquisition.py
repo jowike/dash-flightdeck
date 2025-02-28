780b019f190d036973f0d68589afc5d33c14d61d
jowike <jowik.elzbieta@gmail.com>
2025-02-28 22:19:58 +0100

    add new kpis

--- pages/dashboard/acquisition.py
+++ pages/dashboard/acquisition.py
@@ -9,33 +9,36 @@ def acquisition():
                 html.H2("Model Assessment", className='fs-5 fw-bold mb-1'),
                 html.P("Gives you an overview of the key evaluation metrics for predictive analytics to effectively assess the quality of nowcasts."),
                 html.Div([
-                    html.Div([
+                     html.Div([
                         html.Div([
-                            ICON.TRIANGLE_EXCLAMATION
-                       ], className='icon-shape icon-sm icon-shape-danger rounded me-3',
-                        ),
-                        html.Div([
-                            html.Label("Average Error Rate", className='mb-0'),
-                            html.H4("Not Available", className='mb-0', id='average-error-rate')
-                        ], className='d-block')
-                    ], className='d-flex align-items-center me-5'),
-                    html.Div([
-                        html.Div([
-                            ICON.FIRE.ME2
+                            ICON.CHART_LINE
                         ], className='icon-shape icon-sm icon-shape-secondary rounded me-3',
-                        style={'background-color': '#FBEAD6', 'color': '#FFC900'},  # F8BD7A
-                        # style={'background-color': '#ECEEEB', 'color': '#464A4D'}
+                        style={'background-color': '#e6f6eb', 'color': '#96c8a2'}
                         ),
                         html.Div([
                             html.Label("Adjusted R-Squared", className='mb-0'),
                             html.H4("Not Available", className='mb-0', id='adjusted-r-squared')
                         ], className='d-block')
+                    ], className='d-flex align-items-center me-5'),
+                    html.Div([
+                        html.Div([
+                            ICON.TRIANGLE_EXCLAMATION
+                       ], className='icon-shape icon-sm icon-shape-danger rounded me-3',
+                        style={'color': '#92394b'},  # F8BD7A
+                        ),
+                        html.Div([
+                            html.Label("Average Error Rate", className='mb-0'),
+                            html.H4("Not Available", className='mb-0', id='average-error-rate')
+                        ], className='d-block')
                     ], className='d-flex align-items-center pt-3'),
+                    
                     html.H2("Underlying Estimates", className='fs-6 fw-bold mt-3'),
                     html.Div([
                         html.Div([
                             ICON.GLOBE.ME0
-                        ], className='icon-shape icon-sm icon-shape-tertiary rounded me-3', style={'background-color': '#EAE2E3', 'color': '#50270C'}),
+                        ], className='icon-shape icon-sm icon-shape-secondary rounded me-3',
+                        # style={'background-color': '#e6f6eb', 'color': '#abd2b6'}
+                        ),
                         html.Div([
                             html.Label("Number of Indicators", className='mb-0'),
                             html.H4("Not Available", className='mb-0', id='indicators-count')
@@ -44,14 +47,13 @@ def acquisition():
                     html.Div([
                         html.Div([
                             ICON.HEXAGON
-                        ], className='icon-shape icon-sm icon-shape-purple rounded me-3'),
+                        ], className='icon-shape icon-sm icon-shape-purple rounded me-3',
+                        ),
                         html.Div([
                             html.Label("Number of Models", className='mb-0'),
                             html.H4("Not Available", className='mb-0', id='models-count')
                         ], className='d-block')
                     ], className='d-flex align-items-center pt-3')
-
-
 
 
                 ], className='d-block')
